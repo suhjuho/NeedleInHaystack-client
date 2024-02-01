b43a5a3a244807b3066963e3d7706290bd345de1
pieceofcakey <choi_927@naver.com>
2024-02-01 20:47:33 +0900

    Feat: add react-router

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
-import App from "./App";
+import App from "./components/App";
+import ErrorPage from "./components/ErrorPage";
 import "./index.css";
+
+const router = createBrowserRouter([
+  {
+    path: "*",
+    element: <App />,
+    errorElement: <ErrorPage />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 );
