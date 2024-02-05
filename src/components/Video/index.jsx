b3d7f7b17f984e7684b7c193ddf85ad7c31689c9
pieceofcakey <choi_927@naver.com>
2024-02-05 18:27:02 +0900

    Refactor: no video if it is not available and no tag and transcript if no value

--- src/components/Video/index.jsx
+++ src/components/Video/index.jsx
@@ -1,28 +1,59 @@
+import { useState } from "react";
 import ReactPlayer from "react-player/youtube";
 
 function Video({ video }) {
   const youtubeURL = `https://www.youtube.com/watch?v=${video.youtubeVideoId}`;
+  const [isAvailable, setIsAvailable] = useState(true);
+
   return (
-    <>
-      <ReactPlayer
-        style={{
-          marginTop: 80,
-          marginLeft: 16,
-          borderWidth: 2,
-          borderColor: "rgb(100 116 139)",
-        }}
-        width={800}
-        height={450}
-        url={youtubeURL}
-        controls="true"
-      />
-      <div className="flex flex-col w-[800px] mt-2 ml-4 mb-4 p-2 border-2 border-slate-500">
-        <div className="border font-bold">제목: {video.title}</div>
-        <div className="border">설명: {video.description}</div>
-        <div className="border">채널명: {video.channel}</div>
-        <div className="border">태그: {video.tag}</div>
-      </div>
-    </>
+    <div>
+      {isAvailable ? (
+        <>
+          <ReactPlayer
+            style={{
+              marginTop: 80,
+              marginLeft: 16,
+              borderWidth: 2,
+              borderColor: "rgb(100 116 139)",
+            }}
+            width={800}
+            height={450}
+            url={youtubeURL}
+            onError={() => {
+              setIsAvailable(false);
+            }}
+            controls
+          />
+          <div className="flex flex-col w-[800px] mt-2 ml-4 mb-4 p-2 border-2 border-slate-500">
+            <div className="border font-bold">제목: {video.title}</div>
+            <div className="border">설명: {video.description}</div>
+            <div className="border">채널명: {video.channel}</div>
+            {video.tag === " " || (
+              <div className="border">태그: {video.tag}</div>
+            )}
+          </div>
+        </>
+      ) : (
+        <>
+          <img
+            className="w-[800px] h-[450px] mt-20 ml-4 border-2 border-slate-500"
+            src={video.thumbnailURL}
+            alt="thumbnail"
+          />
+          <div className="flex flex-col w-[800px] mt-2 ml-4 mb-4 p-2 border-2 border-slate-500">
+            <div className="border font-bold text-red-500 uppercase ">
+              This video is no longer available and archived by Haystack
+            </div>
+            <div className="border font-bold">제목: {video.title}</div>
+            <div className="border">설명: {video.description}</div>
+            <div className="border">채널명: {video.channel}</div>
+            {video.tag === " " || (
+              <div className="border">태그: {video.tag}</div>
+            )}
+          </div>
+        </>
+      )}
+    </div>
   );
 }
 
--- src/components/VideoScript/index.jsx
+++ src/components/VideoScript/index.jsx
@@ -1,7 +1,11 @@
 function VideoScript({ transcript }) {
   return (
-    <div className="absolute top-[176px] left-[830px] h-[610px] mr-4 p-2 border-2 border-slate-500 text-justify overflow-scroll">
-      {transcript}
+    <div>
+      {transcript === " " || (
+        <div className="absolute top-[176px] left-[830px] h-[610px] mr-4 p-2 border-2 border-slate-500 text-justify overflow-scroll">
+          {transcript}
+        </div>
+      )}
     </div>
   );
 }
