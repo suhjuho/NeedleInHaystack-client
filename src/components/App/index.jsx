23833cf330d5e1be9b0c5acea871a0ea9fd3292c
pieceofcakey <choi_927@naver.com>
2024-02-05 16:19:16 +0900

    Feat: add DetailPage router

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -2,12 +2,14 @@ import { Route, Routes } from "react-router-dom";
 import MainPage from "../MainPage";
 import ErrorPage from "../ErrorPage";
 import ResultPage from "../ResultPage";
+import DetailPage from "../DetailPage";
 
 function App() {
   return (
     <Routes>
       <Route path="/" exact element={<MainPage />} />
       <Route path="/results" element={<ResultPage />} />
+      <Route path="/watch" element={<DetailPage />} />
       <Route path="*" element={<ErrorPage errorMessage="404 Not Found!" />} />
     </Routes>
   );
