782abb0302428035056d3ae7cfd73dbc8ed45fc8
suhjuho <sspaka0422@gmail.com>
2024-02-07 20:07:03 +0900

    Feat: add useFetchAllVideos

--- src/components/ResultPage/index.jsx
+++ src/components/ResultPage/index.jsx
@@ -2,41 +2,21 @@ import { useInfiniteQuery } from "@tanstack/react-query";
 import { useLocation } from "react-router-dom";
 import { useEffect } from "react";
 import { useInView } from "react-intersection-observer";
-import axios from "axios";
 
 import Header from "../Header";
 import SearchInput from "../SearchInput";
 import VideoList from "../VideoList";
 import { Loading, LoadingSpin } from "../shared/Loading";
 
-import CONSTANT from "../../constants/constant";
+import useFetchAllVideos from "../../apis/useFetchAllVideos";
 
 function ResultPage() {
   const location = useLocation();
   const query = location.search.split("?search_query=")[1];
   const { ref, inView } = useInView();
 
-  async function fetchSearchResults({ pageParam }) {
-    const response = await axios.post(
-      `${import.meta.env.VITE_BASE_URL}/keywords/`,
-      {
-        userInput: query.split("+"),
-        shouldSpellCheck: true,
-        pageParam,
-      },
-    );
-
-    return response.data;
-  }
-
   const { data, fetchNextPage, hasNextPage, isFetchingNextPage, status } =
-    useInfiniteQuery({
-      queryKey: ["search", query],
-      queryFn: fetchSearchResults,
-      initialPageParam: 0,
-      getNextPageParam: (lastPage) => lastPage.nextPage,
-      staleTime: CONSTANT.FIVE_MINUTE_IN_MILLISECONDS,
-    });
+    useFetchAllVideos(query);
 
   useEffect(() => {
     if (inView && hasNextPage) {
@@ -44,36 +24,20 @@ function ResultPage() {
     }
   }, [inView, hasNextPage, fetchNextPage]);
 
-  if (status === "pending") {
-    return (
-      <div className="flex flex-col items-center mt-10">
-        <Header />
-        <SearchInput />
-        <Loading />
-      </div>
-    );
-  }
-
-  if (status === "error") {
-    return (
-      <div className="flex flex-col items-center mt-10">
-        <Header />
-        <SearchInput />
+  return (
+    <div className="flex flex-col items-center mt-10">
+      <Header />
+      <SearchInput />
+      {status === "pending" && <Loading />}
+      {status === "error" && (
         <div className="mt-10 text-center font-bold">
           <div className="text-xl">No results found</div>
           <p className="mt-4">
             Try different keywords or remove search filters
           </p>
         </div>
-      </div>
-    );
-  }
-
-  return (
-    <div className="flex flex-col items-center mt-10">
-      <Header />
-      <SearchInput />
-      {data.pages[0].result !== "null" ? (
+      )}
+      {status === "success" && data.pages[0].result !== "null" ? (
         <>
           {data.pages.map((group) =>
             group.videos.map((video) => {
