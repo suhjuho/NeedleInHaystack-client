4ae802f0b373799708f7512128d0cea6940def81
seunggyu <devkim0702@gmail.com>
2024-02-08 03:41:32 +0900

    Feat: add spell checking feature

--- src/components/ResultPage/index.jsx
+++ src/components/ResultPage/index.jsx
@@ -1,4 +1,3 @@
-import { useInfiniteQuery } from "@tanstack/react-query";
 import { useLocation } from "react-router-dom";
 import { useEffect } from "react";
 import { useInView } from "react-intersection-observer";
@@ -9,20 +8,32 @@ import VideoList from "../VideoList";
 import { Loading, LoadingSpin } from "../shared/Loading";
 
 import useFetchAllVideos from "../../apis/useFetchAllVideos";
+import { useCheckSpellStore } from "../../store/store";
 
 function ResultPage() {
   const location = useLocation();
+  const { shouldCheckSpell, setShouldCheckSpell } = useCheckSpellStore();
   const query = location.search.split("?search_query=")[1];
   const { ref, inView } = useInView();
 
   const { data, fetchNextPage, hasNextPage, isFetchingNextPage, status } =
-    useFetchAllVideos(query);
+    shouldCheckSpell
+      ? useFetchAllVideos(query)
+      : useFetchAllVideos(query, false);
 
   useEffect(() => {
     if (inView && hasNextPage) {
       fetchNextPage();
     }
   }, [inView, hasNextPage, fetchNextPage]);
+
+  function handleSearchInsteadClick() {
+    if (shouldCheckSpell) {
+      setShouldCheckSpell(false);
+    } else {
+      setShouldCheckSpell(true);
+    }
+  }
 
   return (
     <div className="flex flex-col items-center mt-10">
@@ -39,6 +50,23 @@ function ResultPage() {
       )}
       {status === "success" && data.pages[0].result !== "null" ? (
         <>
+          {data.pages[0].query !== data.pages[0].correctedInput && (
+            <p className="mt-3 mb-3">
+              Showing results for
+              <span className="font-bold italic ml-2 mr-2" role="button">
+                {data.pages[0].correctedInput}
+              </span>
+              Search instead for
+              <span
+                className="font-bold ml-2 hover:text-purple-900 hover:underline"
+                onClick={handleSearchInsteadClick}
+                role="button"
+                tabIndex={0}
+              >
+                {data.pages[0].query}
+              </span>
+            </p>
+          )}
           {data.pages.map((group) =>
             group.videos.map((video) => {
               const youtubeVideoId = video[0];
@@ -57,9 +85,28 @@ function ResultPage() {
           </div>
         </>
       ) : (
-        <div className="mt-10 text-center font-bold">
-          <div className="text-xl">No results found</div>
-          <p className="mt-4">
+        <div className="mt-3 text-center">
+          {!shouldCheckSpell && (
+            <p className="mb-10 text-4xl">
+              Did you mean
+              <span
+                className="font-bold italic ml-2 hover:text-purple-900 hover:underline"
+                onClick={handleSearchInsteadClick}
+                role="button"
+                tabIndex={0}
+              >
+                {data?.pages[0].recommendedSearchKeyword}
+              </span>
+              ?
+            </p>
+          )}
+          <p className="mb-10">
+            Your search -
+            <span className="font-bold ml-2 mr-2">{data?.pages[0].query}</span>
+            did not match any documents.
+          </p>
+          <div className="text-xl font-bold">No results found</div>
+          <p className="font-bold mt-5">
             Try different keywords or remove search filters
           </p>
         </div>
