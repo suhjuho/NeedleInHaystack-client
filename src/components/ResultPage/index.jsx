cb70a29f1e23a08fed5a42c31c3c5a026be93c3a
suhjuho <sspaka0422@gmail.com>
2024-02-08 20:46:38 +0900

    Fix: handle error result  rendering

--- src/components/ResultPage/index.jsx
+++ src/components/ResultPage/index.jsx
@@ -48,69 +48,78 @@ function ResultPage() {
           </p>
         </div>
       )}
-      {status === "success" && data.pages[0].result !== "null" ? (
-        <>
-          {data.pages[0].query !== data.pages[0].correctedInput && (
-            <p className="mt-3 mb-3">
-              Showing results for
-              <span className="font-bold italic ml-2 mr-2" role="button">
-                {data.pages[0].correctedInput}
+      {status === "success" &&
+        (data.pages[0].result !== "null" ? (
+          <>
+            {data.pages[0].query !== data.pages[0].correctedInput && (
+              <p className="mt-3 mb-3">
+                Showing results for
+                <span className="font-bold italic ml-2 mr-2" role="button">
+                  {data.pages[0].correctedInput}
+                </span>
+                Search instead for
+                <span
+                  className="font-bold ml-2 hover:text-purple-900 hover:underline"
+                  onClick={handleSearchInsteadClick}
+                  role="button"
+                  tabIndex={0}
+                >
+                  {data.pages[0].query}
+                </span>
+              </p>
+            )}
+            {data.pages.map((group) =>
+              group.videos.map((video, index) => {
+                const youtubeVideoId = video[0];
+                if (group.videos.length === index + 1) {
+                  return (
+                    <VideoList
+                      innerRef={ref}
+                      key={youtubeVideoId}
+                      youtubeVideoId={youtubeVideoId}
+                    />
+                  );
+                }
+                return (
+                  <VideoList
+                    key={youtubeVideoId}
+                    youtubeVideoId={youtubeVideoId}
+                  />
+                );
+              }),
+            )}
+            {isFetchingNextPage && <LoadingSpin />}
+            <div>{!hasNextPage ? "Nothing more to load" : "more..."}</div>
+          </>
+        ) : (
+          <div className="mt-3 text-center">
+            {!shouldCheckSpell && (
+              <p className="mb-10 text-4xl">
+                Did you mean
+                <span
+                  className="font-bold italic ml-2 hover:text-purple-900 hover:underline"
+                  onClick={handleSearchInsteadClick}
+                  role="button"
+                  tabIndex={0}
+                >
+                  {data?.pages[0].recommendedSearchKeyword}
+                </span>
+                ?
+              </p>
+            )}
+            <p className="mb-10">
+              Your search -
+              <span className="font-bold ml-2 mr-2">
+                {data?.pages[0].query}
               </span>
-              Search instead for
-              <span
-                className="font-bold ml-2 hover:text-purple-900 hover:underline"
-                onClick={handleSearchInsteadClick}
-                role="button"
-                tabIndex={0}
-              >
-                {data.pages[0].query}
-              </span>
+              did not match any documents.
             </p>
-          )}
-          {data.pages.map((group) =>
-            group.videos.map((video) => {
-              const youtubeVideoId = video[0];
-
-              return (
-                <VideoList
-                  key={youtubeVideoId}
-                  youtubeVideoId={youtubeVideoId}
-                />
-              );
-            }),
-          )}
-          {isFetchingNextPage && <LoadingSpin />}
-          <div ref={ref}>
-            {!hasNextPage ? "Nothing more to load" : "more..."}
+            <div className="text-xl font-bold">No results found</div>
+            <p className="font-bold mt-5">
+              Try different keywords or remove search filters
+            </p>
           </div>
-        </>
-      ) : (
-        <div className="mt-3 text-center">
-          {!shouldCheckSpell && (
-            <p className="mb-10 text-4xl">
-              Did you mean
-              <span
-                className="font-bold italic ml-2 hover:text-purple-900 hover:underline"
-                onClick={handleSearchInsteadClick}
-                role="button"
-                tabIndex={0}
-              >
-                {data?.pages[0].recommendedSearchKeyword}
-              </span>
-              ?
-            </p>
-          )}
-          <p className="mb-10">
-            Your search -
-            <span className="font-bold ml-2 mr-2">{data?.pages[0].query}</span>
-            did not match any documents.
-          </p>
-          <div className="text-xl font-bold">No results found</div>
-          <p className="font-bold mt-5">
-            Try different keywords or remove search filters
-          </p>
-        </div>
-      )}
+        ))}
     </div>
   );
 }
