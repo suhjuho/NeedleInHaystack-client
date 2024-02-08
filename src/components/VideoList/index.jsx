593afee72dbe2dbaa71379b0a45079ff12be85b4
pieceofcakey <choi_927@naver.com>
2024-02-08 15:09:34 +0900

    Feat: video in videoList plays on hover event

--- src/components/VideoList/index.jsx
+++ src/components/VideoList/index.jsx
@@ -1,10 +1,14 @@
-import { useQuery } from "@tanstack/react-query";
+import { useState } from "react";
 import { Link } from "react-router-dom";
+import ReactPlayer from "react-player/youtube";
 
 import useFetchSingleVideo from "../../apis/useFetchSingleVideo";
 
 function VideoList({ youtubeVideoId }) {
+  const [isHover, setIsHover] = useState(false);
+  const [isAvailable, setIsAvailable] = useState(true);
   const { data: video, isFetching } = useFetchSingleVideo(youtubeVideoId);
+  const youtubeURL = `https://www.youtube.com/watch?v=${youtubeVideoId}`;
 
   return (
     <div>
@@ -12,11 +16,42 @@ function VideoList({ youtubeVideoId }) {
         <Link to={`/watch?${video.youtubeVideoId}`} state={{ video }}>
           <div className="flex justify-between w-screen gap-x-6 p-2">
             <div className="flex min-w-0 gap-x-4">
-              <img
-                className="h-[200px] flex-none rounded-md bg-gray-50"
-                src={video.thumbnailURL}
-                alt="thumbnail"
-              />
+              {isAvailable ? (
+                <div
+                  onMouseEnter={(event) => {
+                    setTimeout(() => {
+                      setIsHover(true);
+                    }, 300);
+                    event.stopPropagation();
+                  }}
+                  onMouseLeave={(event) => {
+                    setIsHover(false);
+                    event.stopPropagation();
+                  }}
+                >
+                  <ReactPlayer
+                    style={{
+                      flex: "none",
+                      borderColor: "rgb(100 116 139)",
+                      backgroundColor: "rgb(249 250 251)",
+                    }}
+                    width={355}
+                    height={200}
+                    url={youtubeURL}
+                    playing={isHover}
+                    onError={() => {
+                      setIsAvailable(false);
+                    }}
+                  />
+                </div>
+              ) : (
+                <img
+                  className="h-[200px] flex-none rounded-md bg-gray-50"
+                  src={video.thumbnailURL}
+                  alt="thumbnail"
+                />
+              )}
+
               <div className="min-w-0 flex-auto">
                 <p className="text-lg font-semibold leading-6 text-gray-900">
                   {video.title}
