dfd9d7b0966f8155e2a051f8c64909d316a12b90
pieceofcakey <choi_927@naver.com>
2024-02-13 03:10:18 +0900

    style: change MainPage layout

--- src/components/MainPage/index.jsx
+++ src/components/MainPage/index.jsx
@@ -1,11 +1,32 @@
+import { useEffect } from "react";
+import { Link } from "react-router-dom";
+
 import Header from "../Header";
 import SearchInput from "../SearchInput";
 
+import { useHeaderStateStore } from "../../store/store";
+
 function MainPage() {
+  const { setHeaderState } = useHeaderStateStore();
+
+  useEffect(() => {
+    setHeaderState("MainPage");
+  }, []);
+
   return (
-    <div className="flex flex-col items-center mt-10">
+    <div className="flex flex-col">
       <Header />
-      <SearchInput />
+      <Link to="/">
+        <div className="mt-10 max-w-full items-center text-center text-4xl font-bold">
+          Needle In Haystack
+        </div>
+      </Link>
+      <p className="my-2 text-center font-semibold">
+        Search the video using Haystack!
+      </p>
+      <div className="flex justify-center items-center">
+        <SearchInput />
+      </div>
     </div>
   );
 }
