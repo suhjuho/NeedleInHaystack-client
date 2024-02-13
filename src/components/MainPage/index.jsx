2c082cc967fd06e7879ceb74f8ebe0a5871e0bba
suhjuho <sspaka0422@gmail.com>
2024-02-13 21:55:18 +0900

    Feat: add logo

--- src/components/MainPage/index.jsx
+++ src/components/MainPage/index.jsx
@@ -17,11 +17,12 @@ function MainPage() {
     <div className="flex flex-col">
       <Header />
       <Link to="/">
-        <div className="mt-10 max-w-full items-center text-center text-4xl font-bold">
-          Needle In Haystack
+        <div className="my-10 flex max-w-full items-center justify-center text-center ">
+          <img className="w-10 mx-2" src="/assets/LogoSample2.png" alt="Logo" />
+          <div className="text-4xl font-bold">Needle In Haystack</div>
         </div>
       </Link>
-      <p className="my-2 text-center font-semibold">
+      <p className="mb-10 text-center font-semibold">
         Search the video using Haystack!
       </p>
       <div className="flex justify-center items-center">
