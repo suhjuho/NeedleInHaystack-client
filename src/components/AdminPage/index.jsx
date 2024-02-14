82c589c2bba4e9911d9d0f110cda445323da3e12
suhjuho <sspaka0422@gmail.com>
2024-02-15 03:58:01 +0900

    Refactor: admin cralwer page

--- src/components/AdminPage/index.jsx
+++ src/components/AdminPage/index.jsx
@@ -1,16 +1,29 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import {
+  CheckCircleIcon,
+  ExclamationTriangleIcon,
+  CircleStackIcon,
+} from "@heroicons/react/24/solid";
 
 import { LoadingSpin } from "../shared/Loading";
+import Header from "../Header";
+import { useHeaderStateStore } from "../../store/store";
 
 function AdminPage() {
   const navigate = useNavigate();
   const [isCrawling, setIsCrawling] = useState(false);
+  const [isCorrectUrl, setIsCorrectUrl] = useState(false);
+  const [isStopping, setIsStopping] = useState(false);
   const [entryURL, setEntryURL] = useState("");
   const [crawlingLogList, setCrawlingLogList] = useState([]);
-  const [isCorrectUrl, setIsCorrectUrl] = useState(false);
   const [inputMessage, setInputMessage] = useState("");
+  const { setHeaderState } = useHeaderStateStore();
+
+  useEffect(() => {
+    setHeaderState("AdminPage");
+  }, []);
 
   useEffect(() => {
     async function checkLogin() {
@@ -47,6 +60,7 @@ function AdminPage() {
       eventSource.onmessage = (event) => {
         const crawlingLog = JSON.parse(event.data);
 
+        setIsCrawling(true);
         setCrawlingLogList((crawlingLogList) => [
           ...crawlingLogList,
           crawlingLog,
@@ -64,6 +78,7 @@ function AdminPage() {
 
     return () => {
       clearTimeout(timer);
+      setCrawlingLogList([]);
       eventSource.close();
     };
   }, []);
@@ -74,7 +89,10 @@ function AdminPage() {
       return;
     }
 
+    setCrawlingLogList([]);
     setIsCrawling((isCrawling) => !isCrawling);
+    setIsStopping(false);
+    setInputMessage("");
 
     const response = await axios.get(
       `${import.meta.env.VITE_BASE_URL}/admin/startCrawling/`,
@@ -93,6 +111,8 @@ function AdminPage() {
     if (!isCrawling) {
       return;
     }
+
+    setIsStopping(true);
 
     const response = await axios.get(
       `${import.meta.env.VITE_BASE_URL}/admin/stopCrawling/`,
@@ -145,47 +165,98 @@ function AdminPage() {
   }
 
   return (
-    <div className="flex flex-col justify-contents items-center w-full mt-10">
-      <h1 className="text-2xl">Managing Web Crawler</h1>
-      <form className="flex items-center w-full">
+    <div className="flex flex-col">
+      <Header />
+      <h1 className="w-full px-10 text-4xl font-semibold text-left">
+        Web Crawler
+      </h1>
+      <p className="w-full px-10 my-2 text-secondary-text">
+        A crawler scrap video data from youtube web page, Choose your Entry
+        point for web scraping.
+      </p>
+      <form className="flex justify-start items-center w-full px-10">
         <input
-          onChange={handleInputChange}
-          className="w-full h-[40px] m-2 border-2 border-sky-500"
           type="text"
+          onChange={handleInputChange}
+          className="border border-secondary-border shadow-inner shadow-secondary rounded-lg py-2 px-4 text-xl w-full focus:border-blue-500 outline-none"
+          placeholder="entry url"
         />
-        <button
-          onClick={handleCheckUrl}
-          className={`w-[60px] h-[40px] m-2 rounded-lg ${!isCorrectUrl && !isCrawling ? "bg-orange-300" : "bg-orange-100"}`}
-        >
-          check
-        </button>
-        <button
-          onClick={handleStartCrawling}
-          className={`w-[60px] h-[40px] m-2 rounded-lg ${isCorrectUrl && !isCrawling ? "bg-blue-300" : "bg-blue-100"}`}
-        >
-          start
-        </button>
-        <button
-          onClick={handleStopCrawling}
-          className={`w-[60px] h-[40px] m-2 rounded-lg ${isCrawling ? "bg-red-300" : "bg-red-100"}`}
-        >
-          stop
-        </button>
+        {!isCrawling &&
+          (!isCorrectUrl ? (
+            <button
+              onClick={handleCheckUrl}
+              className="w-32 py-2 px-4 mx-2 rounded-lg text-xl flex-shrink-0 flex items-center justify-center bg-orange-100"
+            >
+              check
+            </button>
+          ) : (
+            <button
+              onClick={handleStartCrawling}
+              className="w-32 py-2 px-4 mx-2 rounded-lg text-xl flex-shrink-0 flex items-center justify-center bg-blue-100"
+            >
+              start
+            </button>
+          ))}
+        {isCrawling && (
+          <button
+            onClick={handleStopCrawling}
+            className="w-32 py-2 px-4 mx-2 rounded-lg text-xl flex-shrink-0 flex items-center justify-center bg-red-100"
+          >
+            stop
+          </button>
+        )}
       </form>
-      {inputMessage && <div>{inputMessage}</div>}
-      <div className="w-full">
-        <div className="h-[800px] m-2 border-2 border-sky-500">
-          {crawlingLogList.length !== 0 &&
-            crawlingLogList.map((crawlingLog) => (
-              <div
-                key={crawlingLog.youtubeVideoId + Date.now()}
-                className="m-2"
-              >
-                {crawlingLog.title}
-              </div>
-            ))}
-          {isCrawling && <LoadingSpin />}
+      {inputMessage && (
+        <p
+          className={`w-full px-10 my-2 text-red-500 ${isCorrectUrl ? "text-green-500" : "text-red-500"}`}
+        >
+          {inputMessage}
+        </p>
+      )}
+      <div className="flex flex-col h-[600px] overflow-auto justify-start items-start w-full gap-y-2 px-10 my-10">
+        <div className="grid gap-4 grid-cols-3 border border-secondary-border shadow-inner shadow-secondary rounded-lg py-2 px-4 text-xl w-full outline-none">
+          <p>state</p>
+          <p>name</p>
+          <p>url</p>
         </div>
+        {crawlingLogList.length !== 0 &&
+          crawlingLogList.map((crawlingLog) => (
+            <div
+              key={crawlingLog.youtubeVideoId + Date.now()}
+              className="grid gap-4 grid-cols-3 border border-secondary shadow-inner shadow-secondary rounded-lg py-2 px-4 text-xl w-full outline-none"
+            >
+              {crawlingLog.result === "ok" &&
+                (crawlingLog.title !== "db" ? (
+                  <>
+                    <p className="flex items-center">
+                      <CheckCircleIcon className="h-[35px] mr-2 fill-green-500" />
+                      {crawlingLog.message}
+                    </p>
+                    <p>{crawlingLog.title}</p>
+                    <p>{crawlingLog.url}</p>
+                  </>
+                ) : (
+                  <p className="flex items-center">
+                    <CircleStackIcon className="h-[35px] mr-2 fill-yellow-500" />
+                    {crawlingLog.message}
+                  </p>
+                ))}
+              {crawlingLog.result === "ng" && (
+                <p className="flex items-center">
+                  <ExclamationTriangleIcon className="h-[35px] mr-2 fill-red-500" />
+                  {crawlingLog.message}
+                </p>
+              )}
+            </div>
+          ))}
+        {isCrawling && (
+          <div className="grid gap-4 grid-cols-3 border border-secondary shadow-inner shadow-secondary rounded-lg py-2 px-4 text-xl w-full outline-none">
+            <div className="flex items-center gap-x-2">
+              <LoadingSpin />
+              {isStopping ? "Stopping" : "Scraping"}
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
