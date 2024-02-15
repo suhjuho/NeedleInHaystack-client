e0a37752f063dddb693db5ddac2bf5a1f36748b9
pieceofcakey <choi_927@naver.com>
2024-02-16 04:38:51 +0900

    style: change Loading components z index to 10

--- src/components/shared/Loading.jsx
+++ src/components/shared/Loading.jsx
@@ -9,7 +9,7 @@ function LoadingSpin() {
 
 function Loading() {
   return (
-    <div className="fixed flex justify-center items-center left-0 right-0 top-0 bottom-0 bg-black-bg bg-opacity-50 z-1">
+    <div className="fixed flex justify-center items-center left-0 right-0 top-0 bottom-0 bg-black-bg bg-opacity-50 z-10">
       <div className="flex flex-col items-center justify-center bg-white rounded-lg p-8">
         <div className="w-8 h-8 relative animate-spin">
           <div className="border-4 border-white border-solid rounded-full absolute top-0 bottom-0 left-0 right-0 animate-spin"></div>
